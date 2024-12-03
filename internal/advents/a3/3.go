6b12ddc236af1cfa282d0bbd3d318b4ec04850f7
Dennis von Bargen (jhn833) <dennis.von.bargen@ica.se>
2024-12-03 16:13:35 +0100

    Added unit test for part 1

--- internal/advents/a3/3.go
+++ internal/advents/a3/3.go
@@ -5,6 +5,19 @@ import internal "dekamik/aoc_2024/internal/command"
 type challenge struct {
 }
 
+type mulArgs struct {
+    a int
+    b int
+}
+
+func mul(args mulArgs) int {
+    return args.a * args.b
+}
+
+func parseInput(s string) []mulArgs {
+    return []mulArgs{}
+}
+
 // ExecutePart1 implements internal.Challenge.
 func (c challenge) ExecutePart1() {
 	panic("unimplemented")
--- internal/advents/a3/3_test.go
+++ internal/advents/a3/3_test.go
@@ -1,1 +1,20 @@
 package a3
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseInput(t *testing.T) {
+	input := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	expected := []mulArgs{
+		mulArgs{a: 2, b: 4},
+		mulArgs{a: 5, b: 5},
+		mulArgs{a: 11, b: 8},
+		mulArgs{a: 8, b: 5},
+	}
+	actual := parseInput(input)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Actual %v not equal to expected %v", actual, expected)
+	}
+}
