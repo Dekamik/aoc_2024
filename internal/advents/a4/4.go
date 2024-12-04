5d81bb9065d4fc21474bda1c195c038576c34c9e
Dennis von Bargen (jhn833) <dennis.von.bargen@ica.se>
2024-12-04 13:43:36 +0100

    Boilerplate for day 4

---
+++ internal/advents/a4/4.go
@@ -0,0 +1,22 @@
+package a4
+
+import internal "dekamik/aoc_2024/internal/command"
+
+type day4 struct {
+}
+
+func countWord(input string, word string) int {
+	panic("unimplemented")
+}
+
+// ExecutePart1 implements internal.Challenge.
+func (d day4) ExecutePart1() {
+	panic("unimplemented")
+}
+
+// ExecutePart2 implements internal.Challenge.
+func (d day4) ExecutePart2() {
+	panic("unimplemented")
+}
+
+var _ internal.Challenge = day4{}
---
+++ internal/advents/a4/4_test.go
@@ -0,0 +1,23 @@
+package a4
+
+import "testing"
+
+func TestCountWord(t *testing.T) {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
+    expected := 18
+    actual := countWord(input, "XMAS")
+
+    if actual != expected {
+		t.Errorf("Actual %v not equal to expected %v", actual, expected)
+    }
+}
