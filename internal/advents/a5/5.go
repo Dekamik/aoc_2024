0895434bf8ad60098f4edb52f785ae1cfb4fef46
Dennis von Bargen (jhn833) <dennis.von.bargen@ica.se>
2024-12-09 09:42:08 +0100

    Added unit tests for day 5

--- internal/advents/a5/5.go
+++ internal/advents/a5/5.go
@@ -5,6 +5,14 @@ import internal "dekamik/aoc_2024/internal/command"
 type day5 struct {
 }
 
+func getCorrectlyOrderedUpdates(input string) []int {
+    panic("unimplemented")
+}
+
+func sumMedian(input [][]int) int {
+    panic("unimplemented")
+}
+
 // ExecutePart1 implements internal.Challenge.
 func (d day5) ExecutePart1() {
 	panic("unimplemented")
--- internal/advents/a5/5_test.go
+++ internal/advents/a5/5_test.go
@@ -1,1 +1,63 @@
 package a5
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetCorrectlyOrderedUpdates(t *testing.T) {
+	input := `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+	expected := [][]int{
+		{75, 47, 61, 53, 29},
+		{97, 61, 53, 29, 13},
+		{75, 29, 13},
+	}
+	actual := getCorrectlyOrderedUpdates(input)
+
+    if !reflect.DeepEqual(actual, expected) {
+        t.Errorf("Expected %v but got %v", expected, actual)
+    }
+}
+
+func TestSumMedian(t *testing.T) {
+    input := [][]int{
+		{75, 47, 61, 53, 29},
+		{97, 61, 53, 29, 13},
+		{75, 29, 13},
+	}
+
+    expected := 143
+    actual := sumMedian(input)
+
+    if actual != expected {
+        t.Errorf("Expected %v but got %v", expected, actual)
+    }
+}
