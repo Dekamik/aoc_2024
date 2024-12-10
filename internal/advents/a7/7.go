7bed9563b5ef7e34026ae1e1b9ab6898bf0b6193
Dennis von Bargen (jhn833) <dennis.von.bargen@ica.se>
2024-12-10 09:26:49 +0100

    Added boilerplate for day 7

---
+++ internal/advents/a7/7.go
@@ -0,0 +1,22 @@
+package a7
+
+import internal "dekamik/aoc_2024/internal/command"
+
+type day7 struct {
+}
+
+// ExecutePart1 implements internal.Challenge.
+func (d day7) ExecutePart1() {
+	panic("unimplemented")
+}
+
+// ExecutePart2 implements internal.Challenge.
+func (d day7) ExecutePart2() {
+	panic("unimplemented")
+}
+
+var _ internal.Challenge = day7{}
+
+func New() internal.Challenge {
+    return day7{}
+}
