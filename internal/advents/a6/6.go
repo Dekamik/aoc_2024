394e849e2e9072129f19b7ca32b97f3365756c5e
Dennis von Bargen (jhn833) <dennis.von.bargen@ica.se>
2024-12-10 10:53:01 +0100

    Began on day 6

--- internal/advents/a6/6.go
+++ internal/advents/a6/6.go
@@ -5,6 +5,41 @@ import "dekamik/aoc_2024/internal/structure"
 type day6 struct {
 }
 
+type tileType int
+
+const (
+	GROUND tileType = iota
+	OBSTRUCTION
+)
+
+type tile struct {
+    Type tileType
+}
+
+func parseTile(r rune) tileType {
+    switch r {
+    case '#':
+        return OBSTRUCTION
+
+    case '^':
+        fallthrough
+    case '<':
+        fallthrough
+    case '>':
+        fallthrough
+    case 'v':
+        fallthrough
+    case '.':
+        fallthrough
+    default:
+        return GROUND
+    }
+}
+
+func calculateDistinctTiles(input string) int {
+    panic("unimplemented")
+}
+
 // ExecutePart1 implements structure.Challenge.
 func (d day6) ExecutePart1() {
 	panic("unimplemented")
@@ -18,5 +53,5 @@ func (d day6) ExecutePart2() {
 var _ structure.Challenge = day6{}
 
 func New() structure.Challenge {
-    return day6{}
+	return day6{}
 }
--- internal/advents/a6/6_test.go
+++ internal/advents/a6/6_test.go
@@ -1,1 +1,23 @@
 package a6
+
+import "testing"
+
+func TestCalculateDistinctTiles(t *testing.T) {
+    input := `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
+
+    expected := 41
+    actual := calculateDistinctTiles(input)
+
+    if actual != expected {
+        t.Errorf("Expected %v, but got %v", expected, actual)
+    }
+}
